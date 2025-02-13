add9efa38cd513b15a24fed97da3ef9a9739d3b1
david-kasprak <kasprakdavid@gmail.com>
2025-02-13 23:44:17 +0200

    trying to run server with docker

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:20-alpine
+
+RUN mkdir /app
+WORKDIR /app
+COPY package.json package-lock.json ./
+RUN npm i
+COPY . .
+
+CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -5,16 +5,15 @@ const app = express();
 
 app.get('/', (req, res) => {
     const hostname = os.hostname();
-
     const ip = require('dns').lookup(hostname, (err, address) => {
         if (err) {
-            res.status(500).send('Error IP');
+            res.status(500).send('Error IP')
         } else {
-            res.send(`${hostname} -- ${address}`)
+            res.send(`${hostname} -- ${address} NODE JS`)
         }
     })
 })
 
-app.listen(3000, "0.0.0.0", () => {
-    console.log('server is running on 3000');
+app.listen(80, "0.0.0.0",() => {
+    console.log('server is running on 80')
 })
